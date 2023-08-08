503bded8c472879ac2c84d3696f4a64fd123c549
TuShaR <tusharkaira456@gmail.com>
2023-08-08 18:04:35 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+
+import Navbar from '@/app/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className='max-w-3xl mx-auto p-4'>
+      <Navbar />
+      <div className='mt-8'>{children}</div>
+      
+      </div>
+      </body>
     </html>
   )
 }
